85e1639dfe10f47c4bfd9124ff0397dc1d41ba12
Arash Rouhani <arash@spotify.com>
2012-07-16 15:24:37 +0200

    Revert "Prettify with the new hash syntax"
    I did this to be compatible with ruby 1.8
    
    This reverts commit 53de9bd0a773179077079f7c1ef4b1feb2232950.

--- korning-selenium.rb
+++ korning-selenium.rb
@@ -10,26 +10,26 @@ driver.navigate.to "https://fp.vv.se/bookingpublic/logon/Privatistbokning.aspx"
 credentials = YAML.load_file 'user.yml'
 
 credentials.each { |key, value|
-  element = driver.find_element(id: "ctl00_ContentPlaceHolder1_txt#{key}")
+  element = driver.find_element(:id, "ctl00_ContentPlaceHolder1_txt#{key}")
   element.send_keys value
 }
 
-driver.find_element(class: "EAbutton").click # Skicka credentials
+driver.find_element(:class, "EAbutton").click # Skicka credentials
 
-omboka_element = driver.find_elements(id: "ctl00_ContentMain_ButtonRescheduleBooking").first
+omboka_element = driver.find_elements(:id, "ctl00_ContentMain_ButtonRescheduleBooking").first
 
 if omboka_element
   id = "ctl00_ContentMain_RepeaterCurrentBookings_ctl01_RadioButtonBooking"
-  driver.find_element(id: id).click # Assume there is only one option and you want to rebook it
+  driver.find_element(:id, id).click # Assume there is only one option and you want to rebook it
   omboka_element.click
 else
   # The user probably doesn't have booked yet, so lets click buttons for booking
-  driver.find_element(id: "ctl00_ContentMain_ButtonBook").click #
-  driver.find_element(id: "ctl00_ContentMain_ButtonNext").click # Typ B körkort
-  driver.find_element(id: "ctl00_ContentMain_ButtonNext").click # Körprov B
+  driver.find_element(:id, "ctl00_ContentMain_ButtonBook").click #
+  driver.find_element(:id, "ctl00_ContentMain_ButtonNext").click # Typ B körkort
+  driver.find_element(:id, "ctl00_ContentMain_ButtonNext").click # Körprov B
 
-  select = driver.find_element(id: "ctl00_ContentMain_ddlCounties")
-  all_options = select.find_elements(tag_name: "option")
+  select = driver.find_element(:id, "ctl00_ContentMain_ddlCounties")
+  all_options = select.find_elements(:tag_name, "option")
   all_options.each do |option|
     value = option.attribute("value")
     if value == "1" # Select 1, which is Stockholm
@@ -37,21 +37,21 @@ else
       break
     end
   end
-  driver.find_element(id: "ctl00_ContentMain_RadioButtonCounty").click # Länets tio snabbaste
-  driver.find_element(id: "ctl00_ContentMain_btnShow").click # Click "Visa"
+  driver.find_element(:id, "ctl00_ContentMain_RadioButtonCounty").click # Länets tio snabbaste
+  driver.find_element(:id, "ctl00_ContentMain_btnShow").click # Click "Visa"
 
 end
 
 i = 0
 while true
   puts "Attempt #{i+=1}:"
-  labelss = driver.find_elements(tag_name: "label")[(omboka_element ? 5 : 0)..-1].each_slice(6)
+  labelss = driver.find_elements(:tag_name, "label")[(omboka_element ? 5 : 0)..-1].each_slice(6)
   raw_lines = labelss.map do |labels|
     labels.map(&:text).join(" ")
   end
   puts raw_lines.join("\n")
   Goer::go(raw_lines)
   sleep(30)
-  driver.find_element(id: "ctl00_ContentMain_OccasionList1_btnUpdateOccasions").click
+  driver.find_element(:id, "ctl00_ContentMain_OccasionList1_btnUpdateOccasions").click
 end
 
